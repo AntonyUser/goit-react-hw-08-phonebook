7bd65962ba743fa670ccdecaf015eb725e04aaba
AntonyUser <101988185+AntonyUser@users.noreply.github.com>
2022-12-28 12:55:28 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>
-        Cotacts saver welcome page{' '}
+        Contacts saver welcome page{' '}
         <span role="img" aria-label="Greeting icon">
           💁‍♀️
         </span>
